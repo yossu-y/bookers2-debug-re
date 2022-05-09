4827d23433719f6bfcc0fe36d2cf4f341e12b507
yossu-y <yangyuub@gmail.com>
2022-05-09 14:14:51 +0000

    残りviewファイルの更新

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,11 @@ Rails.application.routes.draw do
     resource :favorite, only: [:create, :destroy]
   end
 
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource relationships, only: [:create, :destroy]
+    get "followings" => "relationships#followings", as: "followings"
+    get "followers" => "relationships#followers", as: "followers"
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
