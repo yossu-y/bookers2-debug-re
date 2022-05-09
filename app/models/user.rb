e960553ba1d7b1201a32e36c3cf842fdafece9c5
yossu-y <yangyuub@gmail.com>
2022-05-09 13:58:53 +0000

    [add]model.controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,24 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
 
   has_one_attached :profile_image
+  
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  
+  def follow(user_id)
+   relationships.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
