611ebe60aaad301ca462d42220db12e4902d773d
yossu-y <yangyuub@gmail.com>
2022-05-10 07:52:36 +0000

    [finish]いいね非同期

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,14 +4,14 @@ class FavoritesController < ApplicationController
     @book = Book.find(params[:book_id])
     @favorite = current_user.favorites.new(book_id: @book.id)
     @favorite.save
-    render 'replace_btn'
+    # render 'replace_btn'
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     @favorite = current_user.favorites.find_by(book_id: @book.id)
     @favorite.destroy
-    render 'replace_btn'
+    # render 'replace_btn'
   end
 
 end
