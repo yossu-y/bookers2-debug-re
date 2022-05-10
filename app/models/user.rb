34305610a676c8c0d702bdbae2d1585b4a1111ae
yossu-y <yangyuub@gmail.com>
2022-05-10 02:32:04 +0000

    [finish]task4

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,19 +9,20 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
 
   has_one_attached :profile_image
-  
+
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
 
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
-  
-  def follow(user_id)
-   relationships.create(followed_id: user_id)
+
+
+  def follow(user)
+    relationships.create(followed_id: user.id)
   end
 
-  def unfollow(user_id)
-    relationships.find_by(followed_id: user_id).destroy
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
   end
 
   def following?(user)
