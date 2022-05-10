7bbc54522aa5944c89d9e2a2b266f918c0688eb2
yossu-y <yangyuub@gmail.com>
2022-05-10 01:21:25 +0000

    [update]view(訂正必要)

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -6,7 +6,7 @@ class RelationshipsController < ApplicationController
   end
 
   def destroy
-    current_user.unfollowed(params[:user_id])
+    current_user.unfollow(params[:user_id])
     redirect_to request.referer
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'relationships/followings'
-  get 'relationships/followers'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
 
@@ -13,7 +12,7 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:index,:show,:edit,:update] do
-    resource relationships, only: [:create, :destroy]
+    resource :relationship, only: [:create, :destroy]
     get "followings" => "relationships#followings", as: "followings"
     get "followers" => "relationships#followers", as: "followers"
   end
